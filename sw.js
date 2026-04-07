e4f51466cf2f55cfdc34c19940efffe739713d2d
Yakov Forshteer <forshteer@mail.ru>
2026-04-08 00:53:42 +0300

    fix urls for root domain

--- sw.js
+++ sw.js
@@ -1,20 +1,15 @@
-const CACHE = 'planner-v2';
-const ASSETS = ['/planner/', '/planner/index.html', '/planner/manifest.json', '/planner/icon.png'];
-
+const CACHE = 'planner-v3';
+const ASSETS = ['/', '/index.html', '/manifest.json', '/icon.png'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS).catch(() => {})));
   self.skipWaiting();
 });
-
 self.addEventListener('activate', e => {
   e.waitUntil(caches.keys().then(keys =>
     Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
   ));
   self.clients.claim();
 });
-
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => cached))
-  );
+  e.respondWith(caches.match(e.request).then(c => c || fetch(e.request).catch(() => c)));
 });
